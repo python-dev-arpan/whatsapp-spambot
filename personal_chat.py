44173d0d87e11e4f04dbaf10f8979c694acb46e2
Arpan <pythondevarpan@gmail.com>
2023-03-11 16:31:29 +0530

    Update

--- personal_chat.py
+++ personal_chat.py
@@ -5,16 +5,12 @@ import time
 import pyautogui
 
 
-group_invite_link = input("Paste the group invite link: ")
+phone = input("Enter the phone no: ")
 message = input("Enter the message: ")
 times = int(input("How many times? "))
-webbrowser.open(group_invite_link)
+whatsapp_link = f"https://web.whatsapp.com/send?phone={phone}&app_absent=0"
+webbrowser.open(whatsapp_link)
 time.sleep(15)
-pyautogui.click(668, 551, clicks=2)
-time.sleep(15)
-pyautogui.click(670, 424, clicks=2)
-time.sleep(15)
-pyautogui.click(732, 699, clicks=2)
-time.sleep(15)
+
 for i in range(1, times + 1):
     pyautogui.typewrite(f"{message} {str(i)}\n")
